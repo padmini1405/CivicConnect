aac7b2d6aa30b2384802720ad03ec60f29419729
VenkatAkash-7305 <akashvanbu@gmail.com>
2026-03-20 02:55:09 -0700

    Updated Nav Section and Added Statistical Section and major problems section

--- script.js
+++ script.js
@@ -2,12 +2,48 @@ const menuBtn = document.getElementById("menuBtn");
 const closeBtn = document.getElementById("closeBtn");
 const sidebar = document.getElementById("sidebar");
 
-// OPEN MENU
-menuBtn.addEventListener("click", () => {
-    sidebar.style.right = "0";
-});
+menuBtn.onclick = () => sidebar.style.right = "0";
+closeBtn.onclick = () => sidebar.style.right = "-100%";
 
-// CLOSE MENU
-closeBtn.addEventListener("click", () => {
-    sidebar.style.right = "-100%";
-});
+const chart = document.getElementById("issueChart");
+
+        new Chart(chart, {
+            type: "doughnut",
+            data: {
+                labels: [
+                    "Garbage",
+                    "Potholes",
+                    "Streetlights",
+                    "Water Leakages",
+                    "Stray Dogs",
+                    "Others"
+                ],
+                datasets: [{
+                    data: [25, 20, 18, 17, 15, 5],
+                    backgroundColor: [
+                        "#ff6384",
+                        "#36a2eb",
+                        "#ffce56",
+                        "#4bc0c0",
+                        "#9966ff",
+                        "#6b6b6b"
+                    ],
+                    hoverOffset: 14
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                cutout: "65%",
+                plugins: {
+                    legend: {
+                        position: "bottom",
+                        labels: {
+                            font: { size: 14 },
+                            padding: 10,
+                            usePointStyle: true
+                        }
+                    }
+                }
+            }
+        });
